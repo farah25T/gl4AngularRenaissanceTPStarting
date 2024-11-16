24e455e5cfc5d53416ef952d34dd03ccde5d4966
farah25T <farah.toulgui@insat.ucar.tn>
2024-11-16 09:54:01 +0100

    fixed the cvs list with signals

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,29 +1,30 @@
-import { Component, inject } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
-import { ToastrService } from "ngx-toastr";
-import { CvService } from "../services/cv.service";
-import { ListComponent } from "../list/list.component";
-import { CvCardComponent } from "../cv-card/cv-card.component";
-import { EmbaucheComponent } from "../embauche/embauche.component";
-import { UpperCasePipe, DatePipe } from "@angular/common";
+import { Component, effect, inject } from '@angular/core';
+import { Cv } from '../model/cv';
+import { LoggerService } from '../../services/logger.service';
+import { ToastrService } from 'ngx-toastr';
+import { CvService } from '../services/cv.service';
+import { ListComponent } from '../list/list.component';
+import { CvCardComponent } from '../cv-card/cv-card.component';
+import { EmbaucheComponent } from '../embauche/embauche.component';
+import { UpperCasePipe, DatePipe } from '@angular/common';
+import { NewEmbaucheService } from '../services/newEmbauche.service';
 @Component({
-    selector: "app-cv",
-    templateUrl: "./cv.component.html",
-    styleUrls: ["./cv.component.css"],
-    standalone: true,
-    imports: [
-        ListComponent,
-        CvCardComponent,
-        EmbaucheComponent,
-        UpperCasePipe,
-        DatePipe,
-    ],
+  selector: 'app-cv',
+  templateUrl: './cv.component.html',
+  styleUrls: ['./cv.component.css'],
+  standalone: true,
+  imports: [
+    ListComponent,
+    CvCardComponent,
+    EmbaucheComponent,
+    UpperCasePipe,
+    DatePipe,
+  ],
 })
 export class CvComponent {
   private logger = inject(LoggerService);
   private toastr = inject(ToastrService);
-  private cvService = inject(CvService);
+  private cvService = inject(NewEmbaucheService);
 
   cvs: Cv[] = [];
   selectedCv: Cv | null = null;
@@ -31,19 +32,16 @@ export class CvComponent {
   date = new Date();
 
   constructor() {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-      },
+    effect(() => {
+      console.log(
+        'ListComponent constructor',
+        this.cvService.getFakeCvs(),
+        this.cvService.getselectedCv()
+      );
+      this.cvs = this.cvService.getFakeCvs();
+      this.selectedCv = this.cvService.getselectedCv();
     });
-    this.logger.logger("je suis le cvComponent");
-    this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+    this.logger.logger('je suis le cvComponent');
+    this.toastr.info('Bienvenu dans notre CvTech');
   }
 }
--- src/app/cv/services/newEmbauche.service.ts
+++ src/app/cv/services/newEmbauche.service.ts
@@ -3,6 +3,8 @@ import { Cv } from '../model/cv';
 import { SignalService } from 'src/app/services/signal.service';
 
 export interface CvTraitement {
+  cvs: Cv[];
+  selectedCv: Cv | null;
   embauche: Cv[];
 }
 @Injectable({
@@ -11,11 +13,40 @@ export interface CvTraitement {
 export class NewEmbaucheService extends SignalService<CvTraitement> {
   constructor() {
     super({
-     
+      selectedCv: null,
+      cvs: [
+        new Cv(1, 'aymen', 'sellaouti', 'teacher', 'as.jpg', '1234', 40),
+        new Cv(2, 'skander', 'sellaouti', 'enfant', '       ', '1234', 4),
+      ],
       embauche: [],
     });
   }
 
+  /**
+   *
+   * Retourne un liste fictive de cvs
+   *
+   * @returns CV[]
+   *
+   */
+  getFakeCvs(): Cv[] {
+    return this.state().cvs;
+  }
+
+  /**
+   * Permet d'ajouter un cv au flux des cvs sélectionnés
+   *
+   * @param cv : Le cv à ajouter dans le flux des cvs sélectionnés
+   */
+  selectCv(cv: Cv) {
+    if (cv !== null) {
+      this.set('selectedCv', cv);
+    }
+  }
+
+  getselectedCv(): Cv | null {
+    return this.state().selectedCv;
+  }
   /**
    * Retrieves the list of embauchees from the current state.
    * This provides a snapshot of the current value.
